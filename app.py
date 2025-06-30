1c991bf453f0cdbf01aaa388333e5b8a3b64cb09
SIDDHARTH R <dgsid2626@gmail.com>
2025-06-30 16:08:51 +0530

    Added CSS and delete option

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template, request
-from parser import parse_expenses
+from flask import Flask, render_template, request, redirect, url_for
+from database import init_db, insert_expense, get_expenses_for_today, delete_expense
+from datetime import datetime
+from database import delete_expense
+
+
 
 app = Flask(__name__)
+
+@app.route('/delete/<int:expense_id>')
+def delete(expense_id):
+    delete_expense(expense_id)
+    return redirect(url_for('home'))
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        raw_input = request.form['expense_input']
-        parsed_expenses = parse_expenses(raw_input)
-        return render_template('index.html', submitted=True, expenses=parsed_expenses)
+        category = request.form['category']
+        amount = int(request.form['amount'])
+        date = datetime.today().strftime('%Y-%m-%d')
+        insert_expense(amount, category, date)
+        return redirect(url_for('home'))  
 
-    return render_template('index.html', submitted=False)
+    today = datetime.today().strftime('%Y-%m-%d')
+    today_expenses = get_expenses_for_today(today)
+    total = sum(int(e[2]) for e in today_expenses)
+
+    return render_template('index.html', submitted=True, expenses=today_expenses, total=total)
+
+
+
+
 
 if __name__ == '__main__':
+    init_db()  
     app.run(debug=True)
+
---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount INTEGER,
+            category TEXT,
+            date TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def insert_expense(amount, category, date):
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO expenses (amount, category, date) VALUES (?, ?, ?)',
+              (amount, category, date))
+    conn.commit()
+    conn.close()
+
+def get_expenses_for_today(date):
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor()
+    c.execute('SELECT id, category, amount FROM expenses WHERE date = ?', (date,))
+    data = c.fetchall()
+    conn.close()
+    return data
+
+def delete_expense(expense_id):
+    conn = sqlite3.connect('expenses.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM expenses WHERE id = ?', (expense_id,))
+    conn.commit()
+    conn.close()
+
